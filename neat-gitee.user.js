60f8e47d6d72d5b25dc52e1009ba62fda9624692
LinHe <LinHe.Lurking@gmail.com>
2022-11-09 10:30:01 +0800

    Update neat-gitee.user.js

--- neat-gitee.user.js
+++ neat-gitee.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neat Gitee
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       LinHeLurking
 // @match        https://gitee.com/
@@ -13,36 +13,55 @@
 /* global $ */
 
 (function () {
-  'use strict';
+    'use strict';
 
-  const removeWechatBanner = () => { $(".wechat-banner").remove(); };
+    const removeWechatBanner = () => { $(".wechat-banner").remove(); };
 
-  // Your code here...
-  const clean = () => {
-    if (window.location.href == "https://gitee.com/") {
-      removeWechatBanner();
-      $(".recommend-container").remove();
-      $(".my-star-collection-popup").remove();
-      $("#event-timeline-app")[0].style.width = "980px";
-      $(".rc-users__container")[0].style.marginLeft = "0";
-    } else if (window.location.href.startsWith("https://gitee.com/notifications/")) {
-      removeWechatBanner();
-      const tabs = $("#notice-app > div.notice-actionbar.ui.secondary.pointing.menu > a.item");
-      for (let index = 0; index < tabs.length; index++) {
-        const element = tabs[index];
-        element.addEventListener("click", removeWechatBanner);
-      }
-    }
-    const sideWechatBox = $(".site-wechat-guide");
-    if (sideWechatBox) {
-      sideWechatBox.remove();
+    // Your code here...
+    const removePhoneBindNoti = () => {
+        const phoneBindingNoti = $("#users-dashboard > div.ui.dimmer.modals.page.transition.visible.active > div > div.header");
+        if (phoneBindingNoti && phoneBindingNoti[0].innerHTML.includes("绑定手机")) {
+            const closeButton = $("#users-dashboard > div.ui.dimmer.modals.page.transition.visible.active > div > i");
+            if (closeButton) {
+                closeButton.trigger("click");
+            }
+        }
     }
 
-    const redBadge = $(".label.red.ui");
-    if (redBadge) {
-      redBadge.remove();
-    }
-  };
+    const clean = () => {
+        if (window.location.href == "https://gitee.com/") {
+            removeWechatBanner();
+            $(".recommend-container").remove();
+            $(".my-star-collection-popup").remove();
+            $("#event-timeline-app")[0].style.width = "980px";
+            $(".rc-users__container")[0].style.marginLeft = "0";
 
-  $("documnet").ready(clean);
+            settimeout(removePhoneBindNoti, 1000);
+
+        } else if (window.location.href.startsWith("https://gitee.com/notifications/")) {
+            removeWechatBanner();
+            const tabs = $("#notice-app > div.notice-actionbar.ui.secondary.pointing.menu > a.item");
+            for (let index = 0; index < tabs.length; index++) {
+                const element = tabs[index];
+                element.addEventListener("click", removeWechatBanner);
+            }
+        }
+
+        const sideWechatBox = $(".site-wechat-guide");
+        if (sideWechatBox) {
+            sideWechatBox.remove();
+        }
+
+        const redBadge = $(".label.red.ui");
+        if (redBadge) {
+            redBadge.remove();
+        }
+
+        const discountBadge = $("#git-header-nav > div > div > a:nth-child(5) > img");
+        if (discountBadge) {
+            discountBadge.remove();
+        }
+    };
+
+    $("documnet").ready(clean);
 })();
