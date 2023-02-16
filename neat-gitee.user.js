e429deff0df8aec944ecd7d72427fc1f03788867
LinHe <LinHe.Lurking@gmail.com>
2023-02-16 15:38:37 +0800

    Better recoginition

--- neat-gitee.user.js
+++ neat-gitee.user.js
@@ -17,26 +17,28 @@
 
     const removeWechatBanner = () => { $(".wechat-banner").remove(); };
 
-    // Your code here...
-    const removePhoneBindNoti = () => {
-        const phoneBindingNoti = $("#users-dashboard > div.ui.dimmer.modals.page.transition.visible.active > div > div.header");
-        if (phoneBindingNoti && phoneBindingNoti[0].innerHTML.includes("绑定手机")) {
-            const closeButton = $("#users-dashboard > div.ui.dimmer.modals.page.transition.visible.active > div > i");
-            if (closeButton) {
-                closeButton.trigger("click");
-            }
-        }
-    }
+    const removeIfPresent = (elem) => {
+      if(elem) {
+        elem.remove();
+      }
+    };
 
     const clean = () => {
         if (window.location.href == "https://gitee.com/") {
             removeWechatBanner();
-            $(".recommend-container").remove();
-            $(".my-star-collection-popup").remove();
-            $("#event-timeline-app")[0].style.width = "980px";
-            $(".rc-users__container")[0].style.marginLeft = "0";
+            removeIfPresent($(".recommend-container"));
+            removeIfPresent($(".my-star-collection-popup"));
+            removeIfPresent($(".my-star-collection-popup"));
 
-            setTimeout(removePhoneBindNoti, 600);
+            let tmp = $("#event-timeline-app");
+            if(tmp && tmp.leng > 0) {
+                tmp[0].style.width = "980px";
+            }
+
+            tmp = $(".rc-users__container");
+            if(tmp && tmp.leng > 0) {
+                tmp[0].style.marginLeft = "0";
+            }
 
         } else if (window.location.href.startsWith("https://gitee.com/notifications/")) {
             removeWechatBanner();
@@ -48,18 +50,22 @@
         }
 
         const sideWechatBox = $(".site-wechat-guide");
-        if (sideWechatBox) {
+        if (sideWechatBox && sideWechatBox.length > 0) {
             sideWechatBox.remove();
         }
 
         const redBadge = $(".label.red.ui");
-        if (redBadge) {
+        if (redBadge && redBadge.length > 0) {
             redBadge.remove();
         }
 
-        const discountBadge = $("#git-header-nav > div > div > a:nth-child(5) > img");
-        if (discountBadge) {
-            discountBadge.remove();
+
+        const discountBadge = $("sup");
+        for(let index = 0; index < discountBadge.length; index++) {
+            const element = discountBadge[index];
+            if(element && element.innerHTML == "特惠") {
+              element.remove();
+            }
         }
     };
 
